21ba98d895182002b9b43ef9a10a77f3ad168c05
Juha Tauriainen <juha@bin.fi>
2026-04-10 13:38:25 +0300

    Add first react components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+
+import Home from "./components/Home";
+
 const App = (props) => {
   return (
     <>
       <h1 className="hero-title">
         My App {props.name} - {props.toinen}
       </h1>
+      <Home />
     </>
   );
 };
